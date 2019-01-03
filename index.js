64c6f3196dbdd069588c231a51408b43fe6f37f5
Darius McFarland <indycolts1893@gmail.com>
2019-01-03 10:37:54 -0500

    Implemented the Firebase UI Authentication panel and removed ignored files.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
 
+    $(".login-cover").hide();
+  } else {
+    // No user is signed in.
+    $(".login-cover").show();
+  }
+});
+
+$("#logout-button").click(
+    function() {
+        $(".login-cover").show();
+    }
+);
